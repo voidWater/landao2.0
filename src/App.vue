f18fcca98bda581e5bfe677b65b60990363d1966
voidshell <903458283@qq.com>
2020-08-14 18:52:12 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+        <header>
+        <!-- router-link 定义点击后导航到哪个路径下 -->
+          <router-link to="/home">Home</router-link>
+          <router-link to="/about">About</router-link>
+        </header>
+        <!-- 对应的组件内容渲染到router-view中 -->
+        <router-view></router-view>   
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
